06a360d21303cbf2fbe18239426d86c6796dd113
n1ght4ngel19 <levi.hadhazi@gmail.com>
2022-06-13 19:29:52 +0200

    Added 8-directional animation for testing, set max zoom to map width, removed rock interaction since it was bad all the way, and made some updates to the menu.

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -1,21 +1,17 @@
 import { GridEngine } from 'grid-engine';
 import Phaser from 'phaser';
-import { Stage } from './scenes/Stage';
+import { Stage } from './scenes/stage';
 
 const stage = new Stage();
 
 const gameConfig = {
+  gameTitle: 'Electronic Gruntz',
+  gameVersion: '0.0.1',
   type: Phaser.AUTO,
-  autoCenter: true,
-  width: window.innerWidth * window.devicePixelRatio,
-  height: window.innerHeight * window.devicePixelRatio,
-  // BUG: Framerate is too fast, thus most commands get executed multiple times when called, instead of once.
-  // Temporary fix: limited framerate to 10fps, so commands can be tested
-  // Possible fix: disassociate clicks/commands from framerate altogether
-  fps: {
-    target: 10,
-    forceSetTimeOut: true,
-  },
+  width: window.innerWidth,
+  height: window.innerHeight,
+  roundPixels: true,
+  disableContextMenu: true,
   plugins: {
     scene: [
       {
@@ -24,6 +20,14 @@ const gameConfig = {
         mapping: 'gridEngine',
       },
     ],
+  },
+  autoCenter: true,
+  // BUG: Framerate is too fast, thus most commands get executed multiple times when called, instead of once.
+  // Temporary fix: limited framerate to 10fps, so commands can be tested
+  // Possible fix: disassociate clicks/commands from framerate altogether
+  fps: {
+    target: 30,
+    forceSetTimeOut: true,
   },
 };
 
--- src/renderer/scenes/Stage.ts
+++ src/renderer/scenes/Stage.ts
@@ -5,32 +5,34 @@ const searchParams = new URLSearchParams(window.location.search);
 const mapName = String(searchParams.get('stage'));
 
 export class Stage extends Phaser.Scene {
-  
   constructor() {
     super({ key: 'stage'});
   }
 
   gridEngine!: GridEngine;
-  map!: Phaser.Tilemaps.Tilemap;
-  map2!: Phaser.Tilemaps.Tilemap;
   pointer!: Phaser.Input.Pointer;
-  pointerPosition!: Position;
-  // TODO: Set this dynamically
-  tileSize: number = 32;
+  map!: Phaser.Tilemaps.Tilemap;
+  tileSize!: number;
   // TODO: Populate this dynamically
   characterPositions!: Position[];
-  playerPosition!: Position;
-  // layer data
-
+  // Will become obsolete after multiple characters
+  // ? Layer data
 
 
 
   preload(): void {
 
     this.loadMapParts();
+    // TODO: loadSpritesheets();
 
-    // Load tester Grunt image    
-    this.load.image('grunt', 'animations/Grunt.png');
+    this.load.spritesheet('gruntMovement', 'animations/GruntNormalWalk.png', {
+      frameWidth: 48,
+      frameHeight: 48
+    });
+    this.load.spritesheet('gruntIdle', 'animations/GruntNormalIdle.png', {
+      frameWidth: 48,
+      frameHeight: 48
+    });
   }
 
 
@@ -38,57 +40,73 @@ export class Stage extends Phaser.Scene {
 
 
   create(): void {
+    const mainCam = this.cameras.main;
+    const cursors = this.input.keyboard;
 
-    // Ensure that rightclick-menu doesn't appear
-    this.input.mouse.disableContextMenu();
+    cursors.createCursorKeys();
 
-    this.pointer = this.input.activePointer;
-    
+    // TODO: Set zoom automatically to uniform zoom (e.g. 10 tiles wide)
+    mainCam.zoom = 1.75;
+
     this.input.keyboard.on('keydown-ESC', () => {
       window.location.href = 'questz.html';
+
+      // if (!this.scene.isPaused()) {
+      //   this.scene.pause();
+      //   console.log(this.scene)
+      //   // @ts-ignore
+      //   document.getElementById('menu').style.display = 'none';
+      // }
     });
 
 
-    // this.input.keyboard.on('keydown-A', () => {
-      // this.cameras.main.setZoom(1.1);
-    // });
-
-    // this.cameras.main.roundPixels = true;
-
     // TODO: Make zoom smooth
-    // BUG: Can zoom outside the map boundaries
-    // BUG: Cannot move view on the map manually
+    // TODO: Add ability to drag/scroll the map
     this.input.on('wheel', (pointer: any, GameObjects: any, deltaX: number, deltaY: number, deltaZ: number) => {
-      if(deltaY < 0) {
-        this.cameras.main.zoom += .5;
+      if(mainCam.displayWidth < this.map.widthInPixels) {
+        // Zoom out when scrolling up
+        if(deltaY > 0) { mainCam.zoom -= 1/32; }
+        // Zoom in when scrolling down
+        if(deltaY < 0) { mainCam.zoom += 1/32; }
       }
-      if(deltaY > 0) {
-        this.cameras.main.zoom -= .5;
-      }
-    })
-
-
-
-
-
-
-    // Create tilemap
-    // this.map = this.make.tilemap({ key: 'stageRockyRoadz01' });
+    });
 
     this.map = this.makeMapWithLayers();
 
-    // Add tileset images
-    // const tileset1 = this.map.addTilesetImage('TilesetSwitchMarkers', 'tilesetSwitchMarkers');
-    // const tileset2 = this.map.addTilesetImage('NewSet', 'tilesetRockyRoadz');
-    // const tileset3 = this.map.addTilesetImage('TilesetGeneral', 'tilesetGeneral');
-
-    // Create layers
-    // this.map.createLayer('Switch Markers', tileset1);
-    // this.map.createLayer('Base', tileset2);
-    // this.map.createLayer('Manipulables', tileset3);
-
     // Create player sprite
-    const playerSprite = this.add.sprite(0, 0, 'grunt');
+    let playerSprite = this.add.sprite(0, 0, 'gruntIdle');
+
+    // TODO: createAnimations();
+    this.createAnimation('gruntMovement', 'up', 0, 7);
+    this.createAnimation('gruntMovement', 'up-right', 8, 15);
+    this.createAnimation('gruntMovement', 'right', 16, 23);
+    this.createAnimation('gruntMovement', 'down-right', 24, 31);
+    this.createAnimation('gruntMovement', 'down', 32, 39);
+    this.createAnimation('gruntMovement', 'down-left', 40, 47);
+    this.createAnimation('gruntMovement', 'left', 48, 55);
+    this.createAnimation('gruntMovement', 'up-left', 56, 63);
+
+    this.createAnimation('gruntIdle', 'up-idle', 0, 15);
+    this.createAnimation('gruntIdle', 'up-right-idle', 16, 18);
+    this.createAnimation('gruntIdle', 'right-idle', 32, 47);
+    this.createAnimation('gruntIdle', 'down-right-idle', 48, 50);
+    this.createAnimation('gruntIdle', 'down-idle', 64, 79);
+    this.createAnimation('gruntIdle', 'down-left-idle', 80, 82);
+    this.createAnimation('gruntIdle', 'left-idle', 96, 111);
+    this.createAnimation('gruntIdle', 'up-left-idle', 112, 114);
+
+    playerSprite.anims.play('down-idle');
+
+
+
+
+
+
+
+
+
+
+
 
     const gridEngineConfig = {
       // TODO: Add characters dynamically, based on special tiles (SpawnTile, EnemyTile, etc.)
@@ -98,6 +116,7 @@ export class Stage extends Phaser.Scene {
           sprite: playerSprite,
           startPosition: {x: 2, y: 18},
           speed: 4
+          // speed: Math.floor(10/6)
         },
       ],
       numberOfDirections: 8,
@@ -105,65 +124,106 @@ export class Stage extends Phaser.Scene {
 
     this.gridEngine.create(this.map, gridEngineConfig);
 
+    this.gridEngine.movementStarted().subscribe(({ charId, direction }) => {
+      this.gridEngine.getSprite(charId).anims.play(direction);
+    });
+
+    this.gridEngine.movementStopped().subscribe(({ charId, direction }) => {
+      switch(direction) {
+        case 'up': playerSprite.anims.play('up-idle');
+        break;
+        case 'up-right': playerSprite.anims.play('up-right-idle');
+        break;
+        case 'right': playerSprite.anims.play('right-idle');
+        break;
+        case 'down-right': playerSprite.anims.play('down-right-idle');
+        break;
+        case 'down': playerSprite.anims.play('down-idle');
+        break;
+        case 'down-left': playerSprite.anims.play('down-left-idle');
+        break;
+        case 'left': playerSprite.anims.play('left-idle');
+        break;
+        case 'up-left': playerSprite.anims.play('up-left-idle');
+        break;
+        default: throw new Error('Invalid animation.');
+      }
+    });
+
+
+
+
+
+
+
+
+
     // Look into this, maybe not working
     // Possibly together with zoom function?
-    this.cameras.main.setBounds(0, 0, this.map.widthInPixels+64, this.map.heightInPixels+64);
-    this.cameras.main.startFollow(this.gridEngine.getSprite('player'));
+    mainCam.setBounds(0, 0, this.map.widthInPixels, this.map.heightInPixels);
+    // mainCam.setSize(this.game.canvas.width, this.game.canvas.height);
+    mainCam.startFollow(this.gridEngine.getSprite('player'));
   }
 
 
 
 
-  update(): void {
-    
-    // Using worldX and worldY because this way we get the
-    // correct pointer positions even wheen zoomed in/out
-    let pointerX = Math.floor(this.pointer.worldX/32);
-    let pointerY = Math.floor(this.pointer.worldY/32);
-    this.pointerPosition = { x: pointerX, y: pointerY};
-    
-    // TODO: Update all character positions each frame
-    this.playerPosition = this.gridEngine.getPosition('player');
-
-
-    // TODO: Don't create this every frame
-    let cursors = this.input.keyboard;
-    cursors.createCursorKeys();
-
-
+update(): void {
+    // Using worldX and worldY because this way we get the correct pointer positions even when zoomed in/out
+    let pointerX = Math.floor(this.input.activePointer.worldX/32);
+    let pointerY = Math.floor(this.input.activePointer.worldY/32);
+    let pointerPosition = { x: pointerX, y: pointerY};
+    let playerPosition = this.gridEngine.getPosition('player');
     let manipulablesLayerData = this.map.getLayer('Manipulables').data;
 
-    
-    
+    if(this.input.activePointer.isDown) {
+      let targetTile = manipulablesLayerData[pointerY][pointerX];
+
+      if(targetTile === undefined) {
+        console.log('Cannot move outside the map.');
+      }
+      else if(targetTile.properties.interactive) {
+        this.gridEngine.moveTo('player', { x: targetTile.x, y: targetTile.y });
+      //   if(targetTile.properties.interactive) {
+      //     this.interact('player', targetTile);
+      //   }
+      }
+      else {
+        this.gridEngine.moveTo('player', { x: targetTile.x, y: targetTile.y });
+      }
+    }
+
+
+
+
+
+
+
     // TODO: Replace with on.('pointerdown')?
     // Check if user clicked
-    if( this.pointer.isDown ) {
 
-      let pointedTile = manipulablesLayerData[pointerY][pointerX];
-      
+    // if(this.pointer.isDown) {
+    // if(this.pointer.isDown) {
+    //   let targetTile = manipulablesLayerData[pointerY][pointerX];
+
       // BUG: Character automatically moves onto the rock's position, if the user clicked onto that rock before
       // TODO: handleRocks();
       // Check if the player has clicked on a rock
-      if((pointedTile !== null) &&  pointedTile.properties.rock){
+
+      // if((targetTile !== null) && targetTile.properties.rock){
 
         // If character is already adjacent to the rock, don't move, just remove the rock
-        if( this.isAdjacentToTarget('player', this.pointerPosition) ) {
-          this.map.removeTile(manipulablesLayerData[pointerY][pointerX]);
-        }
-        else {
-
-          this.gridEngine.moveTo('player', { x: pointerX, y: pointerY });
-          
-          // Remove the rock only after the player has stopped moving towards it
-          // if( !this.gridEngine.isMoving('player') ) {
-
-          //   this.map.removeTile(manipulablesLayerData[pointerY][pointerX]);
-          // }
-        }
-      } else {
-        this.gridEngine.moveTo('player', { x: pointerX, y: pointerY });
-      }
-    }
+    //     if( this.isAdjacentToTarget('player', pointerPosition) ) {
+    //       this.map.removeTile(manipulablesLayerData[pointerY][pointerX]);
+    //     }
+    //     else {
+    //       // TODO: Remove the rock after the player has stopped moving towards it
+    //       this.gridEngine.moveTo('player', { x: pointerX, y: pointerY })
+    //     }
+    //   } else {
+    //     this.gridEngine.moveTo ('player', { x: pointerX, y: pointerY });
+    //   }
+    // }
   }
   
 
@@ -196,7 +256,6 @@ export class Stage extends Phaser.Scene {
    */
   handleRocks(data: Phaser.Tilemaps.Tile[][]): void {
 
-
   }
 
 
@@ -215,7 +274,7 @@ export class Stage extends Phaser.Scene {
     // let switchMarkersData = this.map.getLayer('Switch Markers').data;
     // let baseData = this.map.getLayer('Base').data;
     // let manipulablesData = this.map.getLayer('Manipulables').data;
-    
+
     for(let i = 0; i < this.map.width; ++i) {
       for(let j = 0; j < this.map.height; ++j) {
         functionToExecute();
@@ -238,24 +297,31 @@ export class Stage extends Phaser.Scene {
 
   /**
    * Checks if the pointer position is adjacent to the character specified by charId.
+   *
+   * @param charId the id of the checked character in GridEngine
+   * @param target the target position
    */
   isAdjacentToTarget(charId: string, target: Position): boolean {
-    
+
     let charPosition = this.gridEngine.getPosition(charId);
     let dx:number = Math.abs(charPosition.x - target.x);
     let dy:number = Math.abs(charPosition.y - target.y);
 
     if(dx <= 1 && dy <= 1) {
       return (dx + dy === 1 || dx + dy === 2);
-    }
-    else {
+    } else {
       return false;
     }
   }
 
+  /**
+   * Creates the map and populates it with the different layers
+   * used for the game logic.
+   */
   makeMapWithLayers(): Phaser.Tilemaps.Tilemap {
-  
-    let newMap = this.make.tilemap({ key: 'stageRockyRoadz01' });
+
+    let newMap = this.make.tilemap({ key: mapName });
+    this.tileSize = newMap.tileWidth;
 
     // Add tileset images
     const tilesetSwitchMarkers = newMap.addTilesetImage('TilesetSwitchMarkers', 'tilesetSwitchMarkers');
@@ -270,13 +336,53 @@ export class Stage extends Phaser.Scene {
     return newMap;
   }
 
+  /**
+   * Preloads the JSON file the map is based on, and also the tileset images
+   * used by the map.
+   */
   loadMapParts(): void {
 
     this.load.tilemapTiledJSON(mapName, `maps/${mapName}.json`);
-    // this.load.tilemapTiledJSON('stageRockyRoadz01', `maps/stageRockyRoadz01.json`);
     this.load.image('tilesetRockyRoadz', 'tilesets/NewSet.png');
     this.load.image('tilesetGeneral', 'tilesets/TilesetGeneral.png');
     this.load.image('tilesetSwitchMarkers', 'tilesets/TilesetSwitchMarkers.png');
   }
+
+  /**
+   * Creates a character animation from the given spritesheet. The name
+   * parameter is prefixed by a direction, e.g. 'left', 'right', 'up-left',
+   * 'down-right', etc.
+   *
+   * @param spritesheet the spritesheet to create the animation from
+   * @param name the name of the animation
+   * @param startFrame the starting frame of the animation
+   * @param endFrame the ending frame of the animation
+   */
+  createAnimation(spritesheet: string, name: string, startFrame: number, endFrame: number) {
+
+    this.anims.create({
+        key: name,
+        frames: this.anims.generateFrameNumbers(spritesheet, {
+            start: startFrame,
+            end: endFrame
+        }),
+        frameRate: 10,
+        repeat:  -1
+    });
+  }
+
+  /**
+   * Handles the logic of an interaction between a character and an
+   * interactive object, such as a rock, brick, hole, etc.
+   *
+   * @param charId the id of the character in GridEngine that is involved in the interaction
+   * @param target the targeted interactive object
+   */
+  interact(charId: string, target: Phaser.Tilemaps.Tile): void {
+    // TODO: Implement
+    this.gridEngine.moveTo(charId, { x: target.x, y: target.y });
+  }
+
+
 
 }
