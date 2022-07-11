0f49b2ef469704ca163c561010db4981b1075060
Hadházi Levente <levi.hadhazi@gmail.com>
2022-07-11 17:03:04 +0200

    Altered stage selection so that the tileset used has to be specified on the stage selection page, this way the used tileset doesn't have to be hardcoded, but loaded according to the stage selection page.

--- src/renderer/StageSelector.ts
+++ src/renderer/StageSelector.ts
@@ -2,6 +2,6 @@ document.addEventListener('click', (event) => {
   const target = event.target as HTMLElement;
 
   if (target.classList.contains('stage-option')) {
-    window.location.href = `stage.html?stage=${target.dataset.stage}`;
+    window.location.href = `stage.html?stage=${target.dataset.stage}&tileset=${target.dataset.tileset}`;
   }
 });
